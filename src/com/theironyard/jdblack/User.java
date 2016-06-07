dd4f2d96e864fc2618de701e7faea4999d4a2f79
jonathandavidblack <jonnybl915@gmail.com>
2016-06-07 12:02:15 -0400

    submit, view, and delete

--- src/com/theironyard/jdblack/User.java
+++ src/com/theironyard/jdblack/User.java
@@ -1,11 +1,14 @@
 package com.theironyard.jdblack;
 
+import java.util.ArrayList;
+
 /**
  * Created by jonathandavidblack on 6/7/16.
  */
 public class User {
     String name;
     String password;
+    ArrayList<Restaurant> restaurants =new ArrayList<>();
 
     public User(String name, String password) {
         this.name = name;
